b9db84d3c7d737228e346418aae0d88cd66f874b
George Erimia <l3ttuceftw@gmil.com>
2024-03-18 19:04:13 +0200

    Installed React APP (frontend) Created Dummy data for testing endpoints

---
+++ src/main/frontend/src/components/ListTodoComponent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ListTodoComponent = () => {
+  // create dummy data
+  const todosDummy = [
+    {
+      id: 1,
+      title: "Todo 1",
+      description: "Description 1",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      description: "Description 2",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      description: "Description 3",
+      completed: false,
+    },
+  ];
+
+  return <div>ListTodoComponent</div>;
+};
+
+export default ListTodoComponent;
